943693f352c382f58a6ab5ff75e862a4f929df5e
Mayur Pawar <mayurpawar@users.noreply.github.com>
2026-08-26 19:23:44 +0530

    CloudCap: Remove hardcoded secret from sql.tf (use a sensitive variable)

--- variables.tf
+++ variables.tf
@@ -9,3 +9,9 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
+
+variable "db_password" {
+  description = "Sensitive value. Provide via TF_VAR_db_password sourced from Secret Manager; never hardcode in the repo."
+  type        = string
+  sensitive   = true
+}
